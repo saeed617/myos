d84d794a72ed53d4eed03301cc1ae73dbf8b7512
saeed <s_tahmasebi@comp.iust.ac.ir>
2019-01-02 01:00:47 +0330

    part 2

--- kernel.cpp
+++ kernel.cpp
@@ -1,6 +1,8 @@
+#include "types.h"
+
 void printf(char* str) 
 { 
-    static unsigned short* VideoMemory = (unsigned short*)0xb8000; 
+    static uint16_t* VideoMemory = (uint16_t*)0xb8000;
  
     for(int i = 0; str[i] != '\0'; ++i) 
         VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i]; 
@@ -19,7 +21,7 @@ extern "C" void callConstructors()
  
  
  
-extern "C" void kernelMain(const void* multiboot_structure, unsigned int /*multiboot_magic*/) 
+extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot_magic*/)  
 { 
     printf("Hello World! --- http://www.AlgorithMan.de"); 
  
